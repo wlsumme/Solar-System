4d296a2a03e17182e49581325af16dedf043f6aa
Willliam <wlsumme@gmail.com>
2024-07-03 15:15:45 -0500

    almost there dont give up

--- SolarSystem/Models/Planets/Mass.cs
+++ SolarSystem/Models/Planets/Mass.cs
@@ -13,19 +13,3 @@ namespace SolarSystem.Models.Planets
 }
 
 
-.solar - system - gif {
-width: 100 %;
-    max - width: 500px;
-margin: 20px auto;
-    border - radius: 10px;
-overflow: hidden;
-    aspect - ratio: 1 / 1;
-    pointer - events: none;
-}
-
-    .solar - system - gif iframe {
-        width: 100 %;
-height: 100 %;
-border: none;
-pointer - events: none;
-    }
